ea4228d361c915f67a0d4bcadda1e3f14c770a65
buscis-c2-l9 <buscis-c2-l9@scc.ad.losrios.edu>
2017-09-25 15:47:53 -0700

    Initial commit

---
+++ app/src/main/java/com/example/buscis_c2_l9/myapplication/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.buscis_c2_l9.myapplication;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
+import android.widget.TextView.OnEditorActionListener;
+import android.view.inputmethod.EditorInfo;
+
+
+public class MainActivity extends AppCompatActivity implements OnClickListener
+{
+    private Button Password;
+    private Button UserName;
+    private EditText UserEnt;
+    private EditText PassEnt;
+    private TextView Verify;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        UserEnt = (EditText) findViewById(R.id.UserEnt);
+        PassEnt = (EditText) findViewById(R.id.PassENt);
+        Password = (Button) findViewById(R.id.Password);
+        UserName = (Button) findViewById(R.id.UserName);
+        Verify = (TextView) findViewById(R.id.Verify);
+
+        PassEnt.setOnClickListener(this);
+        UserEnt.setOnClickListener(this);
+        UserName.setOnClickListener(this);
+        Password.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v)
+    {
+        if (v.getId()==R.id.Password)
+        {
+            if (UserEnt.getText().toString().equals("Admin") && PassEnt.getText().toString().equals("Admin"));
+            {
+               Verify.setText("correct");
+            }
+
+        }
+
+    }
+}
